057e41ee814cc38d94ca365d74bd0c6a7668d12c
Константин Сологуб <kon9397@gmail.com>
2020-10-30 10:19:04 +0200

    add redirection to delete php

--- delete.php
+++ delete.php
@@ -1,36 +1,19 @@
 <?php
 
-session_start();
+    session_start();
 
-include 'cart.php';
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ваш товар удален</title>
-</head>
-<body>
-    <?php
-
+    include 'cart.php';
 
 
     if(isset($_GET['id'])) {
         $id = (int) $_GET['id'];
 
         deleteItem($id);
-        echo '<h1>Товар удален!</h1>';
-        echo '<a href="list.php">Назад</a>';
+
+        header('Location: /list.php');
 
     }
-    ?>
-</body>
-</html>
-
+?>
 
 
 
